89f620d2467bedbff4c26133beaa33679fc6318b
Domen Ramšak <domen.ramsak@gorenje.com>
2023-02-24 12:39:32 +0100

    Small login automation

--- RunTests.py
+++ RunTests.py
@@ -13,13 +13,34 @@ test_cases = {
 	}
 }
 
-def main():
-	# for case in test_cases:
-	# 	site = test_cases[case]["site"]
-	# 	title = test_cases[case]["title"]
-	# 	CheckingSiteTitle(site, title)
+login_values = {
+	"usernamePath" : '/html/body/main/div[2]/div/div/div[2]/div/form/article/div/div[2]/div/div[1]/div[1]/div/div/input',
+    "passwordPath" : '/html/body/main/div[2]/div/div/div[2]/div/form/article/div/div[2]/div/div[1]/div[2]/div[1]/div/input',
+    "submitPath" : '/html/body/main/div[2]/div/div/div[2]/div/form/article/div/div[2]/div/div[2]/div/div[2]/button',
+    "username" : "jedan70743@wireps.com",
+    "password" : "gA5Z5KJxTLsb@u"
+}
 
-	LoginTest()
+test_cases2 = {
+	"case_1": {
+		"site" : "https://si.gorenje.com/login",
+		"values" : login_values
+	}
+}
+
+def main():
+	for case in test_cases:
+		site = test_cases[case]["site"]
+		title = test_cases[case]["title"]
+		CheckingSiteTitle(site, title)
+		print("Test case ", case, " passed")
+
+	for case in test_cases2:
+		site = test_cases2[case]["site"]
+		values = test_cases2[case]["values"]
+		LoginTest(site, values)
+		print("Test case ", case, " passed")
+	
 
 
 if __name__ == "__main__":
--- Web/Live/LoginTest.py
+++ Web/Live/LoginTest.py
@@ -6,21 +6,28 @@ from selenium.webdriver.support import expected_conditions as EC
 from Common.Helpers import *
 
 
-def LoginTest() -> None:
+def LoginTest(site: str, loginValues: dict) -> None:
     print("Test Case 'Login' Started: ")
 
-    usernamePath = '/html/body/main/div[2]/div/div/div[2]/div/form/article/div/div[2]/div/div[1]/div[1]/div/div/input'
-    passwordPath = '/html/body/main/div[2]/div/div/div[2]/div/form/article/div/div[2]/div/div[1]/div[2]/div[1]/div/input'
-    submitPath = '/html/body/main/div[2]/div/div/div[2]/div/form/article/div/div[2]/div/div[2]/div/div[2]/button'
+    # usernamePath = '/html/body/main/div[2]/div/div/div[2]/div/form/article/div/div[2]/div/div[1]/div[1]/div/div/input'
+    # passwordPath = '/html/body/main/div[2]/div/div/div[2]/div/form/article/div/div[2]/div/div[1]/div[2]/div[1]/div/input'
+    # submitPath = '/html/body/main/div[2]/div/div/div[2]/div/form/article/div/div[2]/div/div[2]/div/div[2]/button'
 
-    username = "jedan70743@wireps.com"
-    password = "gA5Z5KJxTLsb@u"
+    # username = "jedan70743@wireps.com"
+    # password = "gA5Z5KJxTLsb@u"
+
+    usernamePath = loginValues["usernamePath"]
+    passwordPath = loginValues["passwordPath"]
+    submitPath = loginValues["submitPath"]
+
+    username = loginValues["username"]
+    password = loginValues["password"]
 
     options = webdriver.ChromeOptions()
     options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     driver = webdriver.Chrome(options=options)
-    driver.get("https://si.gorenje.com/login")
+    driver.get(site)
     
     if(HasCookieMessage(driver)):
         AcceptCookies(driver)
