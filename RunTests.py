d6ca72ad68503cbdc275c50c35e095b4c58971c5
Domen Ramšak <domen.ramsak@gorenje.com>
2023-03-01 13:48:20 +0100

    App testing refactor

--- RunTests.py
+++ RunTests.py
@@ -1,13 +1,15 @@
+from appium import webdriver as appdriver
+from Mobile.AppDriver import *
 from Web.Live.CheckingSiteTitle import *
 from Web.Live.LoginTest import *
 import json
 
 test_cases = {
-	"case_1": {
+	"site_title_1": {
 		"site" : "https://si.gorenje.com/",
 		"title" : "Gorenje | Domača Stran"
 	},
-	"case_2": {
+	"site_title_1": {
 		"site" : "https://si.gorenje.com/",
 		"title" : "Gorenje | Veliki in mali gospodinjski aparati"
 	}
@@ -22,7 +24,7 @@ login_values = {
 }
 
 test_cases2 = {
-	"case_1": {
+	"login_1": {
 		"site" : "https://si.gorenje.com/login",
 		"values" : login_values
 	}
@@ -33,15 +35,28 @@ def main():
 		site = test_cases[case]["site"]
 		title = test_cases[case]["title"]
 		CheckingSiteTitle(site, title)
-		print("Test case ", case, " passed")
+		print("Test case", case, "passed")
 
 	for case in test_cases2:
 		site = test_cases2[case]["site"]
 		values = test_cases2[case]["values"]
 		LoginTest(site, values)
-		print("Test case ", case, " passed")
+		print("Test case", case, "passed")
+
+	# Create options for app testing
+	options = UiAutomator2Options()
+	options.platformVersion = '10'
+	# ADB device UDID
+	options.udid = 'R3CT309J26M'
+	# App .apk file
+	options.app = os.path.abspath(r'.\Mobile\Apps\ConnectLife_1.3.0prod_12300222.apk') 
+	server = 'http://127.0.0.1:4723/wd/hub'
+
+	with AppDriver(options, server) as app:
+		app.TouchButton('//android.widget.Button[@content-desc="NEXT"]')
+		app.driver.swipe(850, 1200, 180, 1200, 400)
+		
+
 	
-
-
 if __name__ == "__main__":
 	main()
