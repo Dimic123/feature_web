fedfe36e29140f4619717e7cb6302a7abe3200aa
Domen Ramšak <domen.ramsak@gorenje.com>
2023-03-23 16:21:57 +0100

    API test update

--- RunTestsAPI.py
+++ RunTestsAPI.py
@@ -6,7 +6,6 @@ def main():
 
 def TestAPI():
     token = AuthAPI("test5@connectlife.io","LJijveUqxQNv3T27RSnhOgkhThYeZYt++WvWgc37ThwNUPziKgVgPMak7oujF2fIOcdiGjQVUMjdYghd9Qj8hA==","5065059336212","07swfKgvJhC3ydOUS9YV_SwVz0i4LKqlOLGNUukYHVMsJRF1b-iWeUGcNlXyYCeK")
-    print(token)
     GetAppliances(token)
 
 if __name__ == "__main__":
--- WebAPI/Swagger/Tests/GetAppliances.py
+++ WebAPI/Swagger/Tests/GetAppliances.py
@@ -1,6 +1,7 @@
 import requests
+import json
 
-def GetAppliances(token: str):
+def GetAppliances(token: str) -> bool:
   url = "https://api.connectlife.io/api/v1/appliance"
 
   payload={}
@@ -8,6 +9,29 @@ def GetAppliances(token: str):
     'Authorization': 'Bearer '+token+''
   }
 
-  response = requests.request("GET", url, headers=headers, data=payload)
+  try:
+    response = requests.request("GET", url, headers=headers, data=payload, timeout=10)
+  except requests.ReadTimeout:
+    print("ERROR - request timeout")
+    return False
 
-  print(response.text)
+  try:
+    data = json.loads(response.text)
+  except:
+    print("ERROR - malformed data")
+    return False
+
+  if "errorMessage" in data:
+    print("ERROR - API call returned error")
+    return False
+
+  if len(data) > 0:
+    if all(k in ["id", "name", "type", "status"] for k in data[0]):
+      print("PASSED - data seems to be formed correctly")
+      return True
+    else:
+      print("ERROR - data structure seems to not be correct")
+      return False
+  else:
+    print("ERROR - missing data")
+    return False
